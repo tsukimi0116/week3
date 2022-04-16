f5ab7a8c7fa2cab27f2a96caaa60da5c3b42d5de
tukimi <yuting870116@gmail.com>
2022-04-17 01:30:57 +0800

    04170130

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,48 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react';
+import '../stylesheets/Home/Home.css';
 
 const Home = () => {
+    const [signIn, setSignIn] = useState({
+        Tris: false,
+        Nono: false,
+        Eric: false,
+        Patrick: false,
+        Yue: false
+    })
+
+    const [deg, setDeg] = useState({
+        Tris: '',
+        Nono: '',
+        Eric: '',
+        Patrick: '',
+        Yue: ''
+    })
+
+    const list = [
+        { num: 1, name: 'Tris', deg: deg.Tris, status: signIn.Tris },
+        { num: 2, name: 'Nono', deg: deg.Nono, status: signIn.Nono },
+        { num: 3, name: 'Eric', deg: deg.Eric, status: signIn.Eric },
+        { num: 4, name: 'Patrick', deg: deg.Patrick, status: signIn.Patrick },
+        { num: 5, name: 'Yue', deg: deg.Yue, status: signIn.Yue },
+    ]
 
     return (
-        <table>
-            <tbody>
-                <tr>
-                    <th>員工編號</th>
-                    <th>員工姓名</th>
-                    <th>是否簽到</th>
-                </tr>
-
-                <tr>
-                    <td>1</td>
-                    <td>Tris</td>
-                    <td>簽到</td>
-                </tr>
-
-                <tr>
-                    <td>2</td>
-                    <td>Nono</td>
-                    <td>簽到</td>
-                </tr>
-
-                <tr>
-                    <td>3</td>
-                    <td>Eric</td>
-                    <td>簽到</td>
-                </tr>
-
-                <tr>
-                    <td>4</td>
-                    <td>Patrick</td>
-                    <td>簽到</td>
-                </tr>
-
-                <tr>
-                    <td>5</td>
-                    <td>Yue</td>
-                    <td>簽到</td>
-                </tr>
-
-            </tbody>
-        </table>
+        <div className='tb'>
+            <table>
+                <tbody>
+                    <tr>
+                        <th>員工編號</th>
+                        <th>員工姓名</th>
+                        <th>體溫</th>
+                        <th>是否簽到</th>
+                    </tr>
+                    {Array.isArray(list) &&
+                        list.map((elm, idx) => (
+                            <tr key={idx}>
+                                <td>{elm.num}</td>
+                                <td>{elm.name}</td>
+                                <td id='deg'>{elm.deg === '' && elm.status ? <input type="text" defaultValue="36.5"
+                                    onKeyDown={(e) => {
+                                        if (e.keyCode === 13) {
+                                            setDeg({ ...deg, [elm.name]: e.target.value })
+                                            setSignIn({ ...signIn, [elm.name]: true })
+                                        }
+                                    }} /> : <span>{elm.deg}</span>}</td>
+                                <td>{elm.deg !== '' ? 'v' : <span id='sign' onClick={() => {
+                                    setSignIn({ ...signIn, [elm.name]: true })
+                                }}>簽到</span>}</td>
+                            </tr>
+                        ))}
+                </tbody>
+            </table>
+        </div>
     )
 }
 
