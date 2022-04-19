0298c0f9c5e3ee2d2f7ac4849df9dcffd92613d7
unknown <nathan.hyt@link8.info>
2022-04-19 17:54:15 +0800

    04191754

--- src/Components/Home/Error404.jsx
+++ src/Components/Home/Error404.jsx
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import num from './num.json'
 
 const Error404 = () => {
+    console.log(num)
+
+    useEffect(() => {
+
+    }, []);
     return (
-        <h1>Error404</h1>
+        <div>
+            <h1>區號</h1>
+            <select name="country" id="">
+                <option value="" disabled >請選擇縣市</option>
+                {Array.isArray(num) &&
+                    num.map((elm, idx) => (
+                        <option key={idx}>{elm.name}</option>
+                    ))
+                }
+            </select>
+
+            {/* <select name="Area" id="">
+                <option value="" disabled >請選擇區域</option>
+                {Array.isArray(num) &&
+                    num.map((elm, idx) => (
+                        <option key={idx}>{elm.districts}</option>
+                    ))
+                }
+            </select> */}
+        </div>
     )
 }
 
